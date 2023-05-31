a84b7315ae177d27f42396397517d164170ec5a8
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-31 17:12:57 +0200

    add comment

--- js/script.js
+++ js/script.js
@@ -5,28 +5,27 @@ console.log('ok js');
 //take element from DOM
 const buttonElement = document.getElementById('check-email');
 const textelement = document.getElementById('test-main');
+// add array
+let emailList = ['peppino@gmail.com', 'gennaro@hotmail.it', 'anselmo@libero.it']
+console.log(emailList);
 //add event listner
 buttonElement.addEventListener('click' , function(){
-    // add array
-    let emailList = ['peppino@gmail.com', 'gennaro@hotmail.it', 'anselmo@libero.it']
-    console.log(emailList);
     //take element from DOM
     const Userelement = document.getElementById('email-input').value;
     console.log(Userelement);
     //add message
+    let messagge = 'Errore 404'
+    //circole check
     for(let i = 0; i < emailList.length; i++ ){
-        let emails;
-        emails = (emailList[i])
-        let message;
-
-        if(Userelement.value != emails){
-            messagge = 'Erorre 404'
-            console.log('carlo')
-        }else if (Userelement === emails){
+        let emails = emailList[i];
+        //check
+        console.log(emails,Userelement.value )
+        //condition if emails are different
+        if(Userelement === emails){
+            //message
             messagge = 'E-mail riconosciuta , siete autorizzati all`accesso';
-        }   
-        textelement.innerText = messagge;
-        
+        }
     }
-
+    //print in page
+    textelement.innerText = messagge;
 })
