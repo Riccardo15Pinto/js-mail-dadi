9cb91cfb82e27e701abd8d988c9c3e6310269ebc
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-31 15:20:52 +0200

    take element from DOM , add array list and add eventlistner to buttom element , (inside) add Userelement

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 ----- EMAIL -----------*/
 //check console
 console.log('ok js');
+//take element from DOM
+const buttonElement = document.getElementById('check-email');
+const textelement = document.getElementById('test-main');
+// add array
+let emailList = ['peppin@gmail.com', 'gennaro@hotmail.it', 'anselmo@libero.it']
+buttonElement.addEventListener('click' , function(){
+
+    const Userelement = document.getElementById('email-input').value;
+    console.log(Userelement);
+})
