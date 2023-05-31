168d2ceb6dd7f2fa01c2041f5a50ddadddad4710
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-31 14:36:40 +0200

    add randomchoice

--- js/main.js
+++ js/main.js
@@ -6,3 +6,6 @@ console.log('ok js');
 // check element
 const buttonElement = document.getElementById('start-game');
 const textElement = document.getElementById('your-result');
+// random variable
+let randomchoice = Math.floor(Math.random() * 6) + 1;
+console.log(randomchoice);
